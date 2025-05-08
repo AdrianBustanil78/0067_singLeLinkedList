3b5c2525d44532b76158cc270a033b2d234f50a2
AdrianBustanil <rianbustanil450@gmail.com>
2025-05-08 20:10:51 +0700

    menambahkan bool delnode

--- singly.cpp
+++ singly.cpp
@@ -67,5 +67,29 @@ public:
     {
         *previous = START;
         *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
     }
+
+    bool delNode(int main)
+    {
+        Node *current, *previous;
+        if (!bsearch(nim, &previous, &current))
+             return false;
+            
+        if (current == START)
+            START = START->next;
+        else 
+           previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+    
 }
