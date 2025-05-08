2c7ddb574bb5e42c03fa87fd9e8ecc351d1fa01b
AdrianBustanil <rianbustanil450@gmail.com>
2025-05-08 20:38:53 +0700

    menampilkan menu dan menampilkan data dalam list

--- singly.cpp
+++ singly.cpp
@@ -113,6 +113,30 @@ public:
 };
 
 int main()
-Linkedlist mhs;
-int nim;
-char ch;
+{
+    Linkedlist mhs;
+    int nim;
+    char ch;
+
+    do  
+    {
+        cout << "Menu" << endl;
+        cout << "1. Membuat data dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+        {
+        else '1':
+        {
+            mhs.addNode():
+            break;
+        }
+        }
+    } 
+}
+
